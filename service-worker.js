71f3bea6b5230a32188f694c1df5d9ffe7dbacd1
nhatlong1 <nhatlong@dytechlab.com>
2023-07-24 18:06:07 +0700

    browser extension

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+﻿var requestFilter = {urls: ["<all_urls>"]};
+var extraInfoSpec = ['extraHeaders','requestBody']; 
+
+chrome.webRequest.onBeforeRequest.addListener(handler, requestFilter, extraInfoSpec);
+async function modifyRequestBody(details) {
+  return new Promise((resolve) => {
+    // Modify the requestBody asynchronously (e.g., await API call)
+    setTimeout(() => {
+      if (details.requestBody && details.requestBody.formData) {
+        // Modify the payload by adding a custom parameter
+        console.log("Body",details.requestBody)
+        console.log("Username",details.requestBody.formData.login)
+        details.requestBody.formData.login = ['sdfsafsdf']
+      }
+      resolve();
+    }, 1000); // Simulate an asynchronous operation with a 1-second delay
+  });
+}
